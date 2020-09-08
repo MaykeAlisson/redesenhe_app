0e81b74d0f5817fdc0fa0e3883ef86a95b26513a
Mayke Alisson <mayke.furtado@arcom.com.br>
2020-09-08 16:34:50 -0300

    feat: criando ui

---
+++ app/src/main/java/br/com/redesenhe/redesenhe/view/ListaObjetivoActivity.kt
@@ -0,0 +1,12 @@
+package br.com.redesenhe.redesenhe.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.redesenhe.redesenhe.R
+
+class ListaObjetivoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_objetivo)
+    }
+}
